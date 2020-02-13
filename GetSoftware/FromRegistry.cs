ad0b4fddbbe60bc1230f51bcbe21c01d18375c7a
Naumov <yaroslav.naumov@office.auriga.msk>
2020-02-13 16:43:51 +0300

    dev

--- GetSoftware/FromRegistry.cs
+++ GetSoftware/FromRegistry.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Management;
-using System.Runtime.InteropServices;
 using Microsoft.Win32;
 
 namespace Agent
--- MyClass.cs
+++ MyClass.cs
@@ -1,5 +1,3 @@
-using System;
-
 namespace Agent
 {
     public class MyClass
